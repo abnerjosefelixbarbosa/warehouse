7ecf43dac4dea5054c6afc21926fafad319bbb60
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-05-08 16:06:41 -0300

    implemented settings

---
+++ backend/src/main/java/com/exemple/backend/configuration/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.exemple.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	@Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.headers()
+            .frameOptions()
+            .disable();
+        http.cors()
+            .and()
+            .csrf()
+            .disable();
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.httpBasic()
+            .and()
+            .authorizeHttpRequests((auth) -> {
+            	auth.anyRequest().permitAll();
+            });
+        return http.build();
+    }
+}
