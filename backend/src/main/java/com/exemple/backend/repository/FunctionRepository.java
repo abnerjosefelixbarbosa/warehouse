6187480e57c8113589d8fb71fcb5424567ca139b
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-05-11 16:53:12 -0300

    implement repositories

--- backend/src/main/java/com/exemple/backend/repository/FunctionRepository.java
+++ backend/src/main/java/com/exemple/backend/repository/FunctionRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.exemple.backend.entity.Function;
 
+@Repository
 public interface FunctionRepository extends JpaRepository<Function, UUID> {
 	boolean existsByFunction(String function);
 	Optional<Function> findByFunction(String function);
