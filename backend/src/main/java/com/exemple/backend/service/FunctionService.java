83b50d8d53311d9a66981c1a0c0b5915f781c869
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-05-11 16:55:05 -0300

    implement service

--- backend/src/main/java/com/exemple/backend/service/FunctionService.java
+++ backend/src/main/java/com/exemple/backend/service/FunctionService.java
@@ -14,6 +14,28 @@ public class FunctionService {
 	@Autowired
 	private FunctionRepository functionRepository;
 	
+	public void save(Function function) {
+		functionRepository.save(function);
+	}
+	
+	public void validCreate(Function function) throws Exception {
+		if (function.getFunction().isEmpty())
+			throw new Exception("function is empty");
+		if (function.getFunction().length() > 30) 
+			throw new Exception("function is greater than 30 characters");
+		if (functionRepository.existsByFunction(function.getFunction()))
+			throw new Exception("function already exists");
+	}
+	
+	public void validUpdata(Function function) throws Exception {
+		if (function.getFunction().isEmpty())
+			throw new Exception("function is empty");
+		if (function.getFunction().length() > 30) 
+			throw new Exception("function is greater than 30 characters");
+		if (functionRepository.existsByFunction(function.getFunction()))
+			throw new Exception("function already exists");
+	}
+	
 	public List<Function> findAll() {
 		return functionRepository.findAll();
 	}
@@ -24,34 +46,6 @@ public class FunctionService {
 	
 	public Function findByFunction(String function) {
 		return functionRepository.findByFunction(function).orElse(null);
-	}
-	
-	public void save(Function function) {
-		functionRepository.save(function);
-	}
-	
-	public void validCreate(Function function) throws Exception {
-		if (function.getFunction().isEmpty()) {
-			throw new Exception("function required");
-		}
-		if (function.getFunction().length() > 30) {
-			throw new Exception("function greater than 30 characters");
-		}
-		if (functionRepository.existsByFunction(function.getFunction())) {
-			throw new Exception("existing function");
-		}
-	}
-	
-	public void validUpdata(Function function) throws Exception {
-		if (function.getFunction().isEmpty()) {
-			throw new Exception("function required");
-		}
-		if (function.getFunction().length() > 30) {
-			throw new Exception("function greater than 30 characters");
-		}
-		if (functionRepository.existsByFunction(function.getFunction())) {
-			throw new Exception("existing function");
-		}
 	}
 	
 	public void deleteById(UUID id) {
